5172179d70d4efbcb35e62e2e8e99068a19ee647
GoldenCreeperJ <jinzetan@foxmail.com>
2025-05-08 23:11:49 +0800

    add more comments

--- util/Converter.py
+++ util/Converter.py
@@ -18,11 +18,13 @@ def jsonl_to_json(input_file, output_file):
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(json_array, f, ensure_ascii=False, indent=4)
 
+    print(f"Converted {input_file} to {output_file}")
+
 
 def yaml_to_json(input_path, output_file):
     """
     Convert a YAML file to a JSON file.
-    Some lack information is added manually from the source code of below link.
+    Some lack information is added manually.
     """
     with open(input_path + "/subject_platforms.yml", 'r', encoding='utf-8') as f:
         platforms = yaml.safe_load(f)
@@ -31,20 +33,22 @@ def yaml_to_json(input_path, output_file):
     with open(input_path + "/subject_staffs.yml", 'r', encoding='utf-8') as f:
         staffs = yaml.safe_load(f)
     result = {
-        "book_series": {0: "单行本", 1: "系列"},
-        "subject_types": {1: "漫画", 2: "动画", 3: "音乐", 4: "游戏", 6: "三次元"},
-        "person_types": {0: "未知", 1: "个人", 2: "公司", 3: "组合"},
-        "character_role": {1: "角色", 2: "机体", 3: "组织", 4: "标志"},
-        "episode_types": {0: "正篇", 1: "特别篇", 2: "OP", 3: "ED", 4: "Trailer", 5: "MAD", 6: "其他"},
-        "character_types": {1: "主角", 2: "配角", 3: "客串"},
-        "subject_platforms": {i: {j: k['type_cn'] for j, k in platforms['platforms'][i].items()} for i in (1, 2, 4, 6)},
-        "subject_relations": {j: k['cn'] for i in relations['relations'].values() for j, k in i.items()},
-        "subject_staffs": {j: k['cn'] for i in staffs['staffs'].values() for j, k in i.items()}}
+        "book_series": {0: "单行本", 1: "系列"},  # Subject series
+        "subject_types": {1: "漫画", 2: "动画", 3: "音乐", 4: "游戏", 6: "三次元"},  # Subject type
+        "person_types": {0: "未知", 1: "个人", 2: "公司", 3: "组合"},  # Person type
+        "character_role": {1: "角色", 2: "机体", 3: "组织", 4: "标志"},  # Character role
+        "episode_types": {0: "正篇", 1: "特别篇", 2: "OP", 3: "ED", 4: "Trailer", 5: "MAD", 6: "其他"},  # Episode type
+        "character_types": {1: "主角", 2: "配角", 3: "客串"},  # Subject-Character type
+        "subject_platforms": {i: {j: k['type_cn'] for j, k in platforms['platforms'][i].items()} for i in (1, 2, 4, 6)},  # Subject platform
+        "subject_relations": {j: k['cn'] for i in relations['relations'].values() for j, k in i.items()},  # Subject-Relation relation-type
+        "subject_staffs": {j: k['cn'] for i in staffs['staffs'].values() for j, k in i.items()}}  # Subject-Person position
     result["subject_platforms"][3] = {0: "其他"}
     result["subject_relations"][4018] = "未知"
     result["subject_relations"][4019] = "未知"
     with open(output_file, 'w', encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=4)
+
+    print(f"Converted {input_path} to {output_file}")
 
 
 def infoBox_to_json(s: str) -> str:
